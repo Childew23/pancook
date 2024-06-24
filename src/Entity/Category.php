795a4013b6f133a62b9e28cf5d94b386735ea6cb
Childew <childeric23@gmail.com>
2024-06-24 17:13:19 +0200

    supression de la colonne parent pour l'entité category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -23,9 +23,6 @@ class Category
     #[ORM\Column(length: 255)]
     private ?string $slug = null;
 
-    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'categories')]
-    private ?self $parent = null;
-
     /**
      * @var Collection<int, self>
      */
@@ -74,17 +71,6 @@ class Category
         return $this->slug;
     }
 
-    public function getParent(): ?self
-    {
-        return $this->parent;
-    }
-
-    public function setParent(?self $parent): static
-    {
-        $this->parent = $parent;
-
-        return $this;
-    }
 
     /**
      * @return Collection<int, self>
@@ -92,28 +78,6 @@ class Category
     public function getCategories(): Collection
     {
         return $this->categories;
-    }
-
-    public function addCategory(self $category): static
-    {
-        if (!$this->categories->contains($category)) {
-            $this->categories->add($category);
-            $category->setParent($this);
-        }
-
-        return $this;
-    }
-
-    public function removeCategory(self $category): static
-    {
-        if ($this->categories->removeElement($category)) {
-            // set the owning side to null (unless already changed)
-            if ($category->getParent() === $this) {
-                $category->setParent(null);
-            }
-        }
-
-        return $this;
     }
 
     /**
