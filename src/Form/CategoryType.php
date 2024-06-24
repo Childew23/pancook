e41856af6833efe6f83ae12633749dc753e532a0
Childew <childeric23@gmail.com>
2024-06-24 17:17:22 +0200

    suppression de parent_id dans category

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -17,12 +16,6 @@ class CategoryType extends AbstractType
         $builder
             ->add('name', TextType::class, [
                 "label" => "Nom de la catégorie",
-            ])
-            ->add('parent', EntityType::class, [
-                'class' => Category::class,
-                'choice_label' => 'name',
-                'placeholder' => 'Sélectionner',
-                'required' => false,
             ])
             ->add('Valider', SubmitType::class)
         ;
