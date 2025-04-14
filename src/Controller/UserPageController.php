8886aec2ce88ce047d5ca67e975bad5fbd47abfb
Childew <childeric23@gmail.com>
2025-04-14 12:10:43 +0200

    interface utilisateur + style remis à jour

--- src/Controller/UserPageController.php
+++ src/Controller/UserPageController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class UserPageController extends AbstractController
 {
-    #[Route('/user/page', name: 'app_user_page')]
-    public function index(): Response
+    #[Route('/user/{username}_{id}', name: 'app_user_page')]
+    public function user(User $user, Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository): Response
     {
+        $likes = $userRepository->getLikes($user->getId());
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'Les informations ont été mises à jour.');
+
+            return $this->redirectToRoute('app_user_page', [
+                'username' => $user->getUsername(),
+                'id' => $user->getId()
+            ]);
+        }
+
         return $this->render('user_page/index.html.twig', [
-            'controller_name' => 'UserPageController',
+            'form' => $form->createView(),
+            'user' => $user,
+            'likes' => $likes
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Il existe déjà un compte lié à cet adresse email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -209,7 +209,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function removeComment(Comment $comment): static
     {
         if ($this->comments->removeElement($comment)) {
-            // set the owning side to null (unless already changed)
             if ($comment->getUser() === $this) {
                 $comment->setUser(null);
             }
