ecb7038d4ca1d5c733c7a2cf15408ffd607b9a2f
Childew <childeric23@gmail.com>
2025-05-26 15:18:31 +0200

    Update CKEditor pour le CRUD

--- src/Form/Admin/EditPostType.php
+++ src/Form/Admin/EditPostType.php
@@ -5,12 +5,13 @@ namespace App\Form\Admin;
 use App\Entity\Post;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
-use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 
 class EditPostType extends AbstractType
@@ -18,30 +19,31 @@ class EditPostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('category', EntityType::class, [
-                'class' => Category::class,
-                'choice_label' => 'name',
-                'label' => 'Catégorie',
-                'placeholder' => 'Sélectionner une catégorie',
+            ->add('title', TextType::class,[
+                "label" => "Titre de l'article",
             ])
-            ->add('content', CKEditorType::class,[
-                "label" => "Contenu",
+            ->add('content', HiddenType::class,[
+                'attr'=>[
+                    'id'=> 'post_content'
+                ]
             ])
             ->add('imageFile', VichImageType::class, [
                 'required' => false,
                 'allow_delete' => true,
                 'delete_label' => 'Supprimer',
-                //'download_label' => '...',
                 'download_uri' => true,
                 'image_uri' => true,
-                //'imagine_pattern' => '...',
                 'asset_helper' => true,
             ])
+            ->add('category', EntityType::class, [
+                "label" => "Catégorie de l'article",
+                'class' => Category::class
+            ])
             ->add('Valider', SubmitType::class)
-            ;
+        ;
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Post::class,
